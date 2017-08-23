0d52976d43c2a1d8ffd17477ff9339094f600517
Tristan <tkidder132@gmail.com>
2017-08-22 23:24:42 -0500

    Frequency bands script initial

---
+++ Assets/_Scripts/ParametricCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ParametricCube : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMultiplier;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioPeer.frequencyBand[band] * scaleMultiplier) + startScale, transform.localScale.z);
+	}
+}
